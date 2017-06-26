9998c732e117292b11efadca30d2f1c3e83ed0ce
thanhcongnguyen <thanhcong150593@gmail.com>
2017-06-26 15:59:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict'
 import  fs from 'fs'
 
-var readFile = async() =>{
-    let data = await fs.readFile('./data.txt','utf-8', (err, result) => {
-        if(err){
-            return err
-        }else{
-            console.log("readFile success")
-            console.log(result)
-            return result
-        }
+function readFile(){
+    return new Promise((resolve, reject)=>{
+        fs.readFile('./data.txt','utf-8', (err, result)=>{
+            if(err) reject(err)
+            resolve(result)
+        })
     })
+}
+
+var test = async() =>{
+    let data = await readFile()
     console.log(data)
 
 }
 
-readFile()
+test()
 
